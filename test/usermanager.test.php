78bb95c8b28d9e17ac8c29184e3939a3ae1380ba
Michael Diamond <dimo414@gmail.com>
2010-07-01 00:47:30 -0700

    Improving UserManager testing

--- test/usermanager.test.php
+++ test/usermanager.test.php
@@ -4,20 +4,55 @@ require_once('../UserManager/UserManager.class.php');
 
 session_start();
 $_user = new UserManager('BasicUserManager.xml');
+
+$user = 'test';
+$pass = 'test';
+$level = UserManager::USER;
+
+$_user->addUser($user,$pass,$level,array());
+$_user->login($user,$pass);
+
+$login_id = $_user->getCurUserID();
+if($login_id <= 0){
+	echo '<p>ERROR: Unexpected user ID</p>';
+	exit;
+}
+
+$login_user = $_user->getCurUsername();
+if($user != $login_user){
+	echo '<p>ERROR: Unexpected username</p>';
+	exit;
+}
+
+$login_level = $_user->getCurUserLevel();
+if($level != $login_level){
+	echo '<p>ERROR: Unexpected level</p>';
+	exit;
+}
+
+$login_user_obj = $_user->getUser($login_id);
+// What to test?
+
+$_user->logout();
+
+if($_user->hasPerm($level)){
+	echo '<p>ERROR: Should have no permissions</p>';
+	exit;
+}
+
+$_user->loginSleep($user,$pass);
+
+if(!$_user->hasPerm($level)){
+	echo '<p>ERROR: Should have permission</p>';
+	exit;
+}
+
+$_user->changePassword($pass,$pass.$pass);
+
+$_user->deleteUser($login_id);
+
 $_user->require_login();
 
-
-
-
-
-
+$_user->manageUsers();
 
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html><head><title>Good</title></head>
-	<body>
-		<h1>All Clear</h1>
-		<p>If you were prompted for a password and then saw this page, you're all set.</p>
-		<p><a href="/clearSession.php">Clear Session</a></p>
-	</body>
-</html>
