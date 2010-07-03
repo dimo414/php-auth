0a30afaabb5603b43ae238e981d93d7da98fd10f
Michael Diamond@Asperatus <dimo414@gmail.com>
2010-07-03 13:18:19 -0700

    initial separation of functionality

--- UserManager/MySQLUserManager.class.php
+++ UserManager/MySQLUserManager.class.php
@@ -21,34 +21,46 @@ class UserManager
   it to the $levels array with the name of the level as the key, and the
   int value of the key as the value.
   */
+  var $file;
+  var $simple;
+  var $dom;
   var $userFields = array();
   var $loginFail = 'Incorrect username or password.';
   var $permissionFail = 'You are logged in, but do not have adequate permissions to view this page.';
   var $levels = array('GUEST' => UserManager::GUEST, 'USER' => UserManager::USER, 'SUPERUSER' => UserManager::SUPERUSER, 'ADMIN' => UserManager::ADMIN);
-  var $user = array(); // populated in constructor
-  var $loadedUser = false;
+  var $fileMD5;
   const GUEST = 0;
   const USER = 2;
   const SUPERUSER = 4;
   const ADMIN = 10;
   
   /*
-  Abstract class that unifies how XMLUserManager and MySQLUserManager behave
+  $file: path to xml file
+  ensure xml file is not publicly accessible
+  
+  Suggest extending this function in order to
+  add additional permission levels and user fields.
   */
   function UserManager($file)
   {
-    if(!isset($_SESSION['usermanager__level']))
-    {
-      $_SESSION['usermanager__id'] = 0;
-      $_SESSION['usermanager__level'] = self::GUEST;
-      $_SESSION['usermanager__username'] = '';
+    $this->file = $file;
+    if(!file_exists($file)){
+    	$success = file_put_contents($file,
+'<?xml version="1.0" encoding="iso-8859-1"?>
+<users>
+  <nextid>1</nextid>
+</users>'
+);
     }
-      
-		$this->user = array(
-			'id' = $_SESSION['usermanager__id'],
-			'level' = $_SESSION['usermanager__level'],
-			'username' = $_SESSION['usermanager__username']
-		);
+    $this->simple = simplexml_load_file($file);
+    $this->fileMD5 = md5($file);
+    
+    if(!isset($_SESSION['usermanager_'.$this->fileMD5.'_level']))
+    {
+      $_SESSION['usermanager_'.$this->fileMD5.'_level'] = self::GUEST;
+      $_SESSION['usermanager_'.$this->fileMD5.'_username'] = '';
+      $_SESSION['usermanager_'.$this->fileMD5.'_id'] = 0;
+    }
   }
   
   //////////////////
@@ -86,18 +98,28 @@ class UserManager
   //////////////////
   
   /*
-  Loads extra details about the current user not stored
-  in session information.  This is anything other than
-  username, level, and id.  The values will be populated
-  in $this->user field.
+  Returns current user's ID.
   */
-	function loadCurUser(){
-		if($this->loadedUser){
-			return;
-		}
-		$this->loadedUser = true;
-		// NOT IMPLEMENTED AT THIS LEVEL
-	}
+  function getCurUserID()
+  {
+    return $_SESSION['usermanager_'.$this->fileMD5.'_id'];
+  }
+  
+  /*
+  Returns current user's username.
+  */
+  function getCurUsername()
+  {
+    return $_SESSION['usermanager_'.$this->fileMD5.'_username'];
+  }
+  
+  /*
+  Returns current user's user level
+  */
+  function getCurUserLevel()
+  {
+    return $_SESSION['usermanager_'.$this->fileMD5.'_level'];
+  }
   
   /*
   $num: numerical user level
@@ -119,17 +141,40 @@ class UserManager
   */
   function getUser($id)
   {
-    // NOT IMPLEMENTED AT THIS LEVEL
+    $users = $this->simple->xpath('user[@id='.htmlentities($id).']');
+    $user = $this->unserializeUser($users[0]);
+    $array = array('username' => (string)$user->username,
+                 'password' => (string)$user->password,
+                 'level'    => (string)$user->level);
+    foreach($this->userFields as $field)
+    {
+      $array[$field] = isset($user->$field) ? (string)$user->$field : '';
+    }
+    return $array;
   }
   
   /*
   $attribute: attribute to lookup from the user fields array (or username, password, or level)
   $value: value of the attribute to look for
   searches for users who have their $attribute set to $value and returns an array of all matches.
+  Note that this is a linear search, and is not efficient enough to reguarly retreive data from.
+  Note also that since this function only returns a id, and not a user's full informaiton, it is not
+  efficent to use this function heavily for search.  A future release will contain a getUserByAttribute()
+  function to limit cycles through the document for cases such as that.
+  
+  Suggest extending this function in order to serialize any attributes you would need serialized.
   */
-  function lookupAttribute($attribute, $value, $justID=false)
+  function lookupAttribute($attribute, $value)
   {
-    // NOT IMPLEMENTED AT THIS LEVEL
+    $array = array();
+    foreach($this->simple->user as $user)
+    {
+      if($user->$attribute == $value)
+      {
+        $array[] = $user['id'];
+      }
+    }
+    return $array;
   }
   
   //////////////////
@@ -145,16 +190,39 @@ class UserManager
   */
   function login($username, $password)
   {
-    $users = $this->lookupAttribute('username',$username);
-    $user = $users[0];
-    if($username == $user['username'] && $user->password == $this->hash($password, $user['password']))
+    // It may be more efficient to XPath the user, and then check the password
+    // Find out if XPath is more efficient than iterating
+    foreach($this->simple->user as $user)
     {
-      $_SESSION['usermanager__level'] = $user['level'];
-      $_SESSION['usermanager__username'] = $user['username'];
-      $_SESSION['usermanager__id'] = $user['id'];
-      return true;
+      if($username == (string)$user->username && $user->password == crypt($password, $user->password))
+      {
+        $_SESSION['usermanager_'.$this->fileMD5.'_level'] = (string)$user->level;
+        $_SESSION['usermanager_'.$this->fileMD5.'_username'] = (string)$user->username;
+        $_SESSION['usermanager_'.$this->fileMD5.'_id'] = (string)$user['id'];
+        return true;
+      }
     }
     return false;
+  }
+  
+  /*
+  $username: username to attempt to log in
+  $password: password to attempt to log in
+  $failcount: number of aceptible failures before sleep() gets called
+  to integrate with your site, pass a username and password entered by a client,
+  if correct, will log user in and return true,
+  if incorrect, will return false AND after $failcount will start
+  adding a sleep call - scaling based on the number of failed
+  attempts - to the execution of the function, delaying future page loads and hindering
+  brute force attacks.
+  */
+  function loginSleep($username, $password, $failcount = 3)
+  {
+  	// must figure out where to get $clientFailCount from...
+  	$clientFailCount = 0;
+  	if($clientFailCount > $failcount)
+  	  sleep(pow(2,$clientFailCount));
+  	return $this->login($username, $password);
   }
   
   /*
@@ -171,20 +239,20 @@ class UserManager
   */
   function require_login($minPermission = UserManager::USER)
   {
-    if($this->user['level'] >= $minPermission)
+    if($this->getCurUserLevel() >= $minPermission)
       return;
       
     $error = false;
     if($_SERVER['REQUEST_METHOD'] == "POST")
     {
-      if($this->login($_POST['user'], $_POST['pass']) && $this->user['level'] >= $minPermission)
+      if($this->login($_POST['user'], $_POST['pass']) && $this->getCurUserLevel() >= $minPermission)
       {
         $_SERVER['REQUEST_METHOD'] = "GET"; // so as not to throw off any other scripts checking against post method
         return;
       }
       else $error = true;
     }
-    if($this->user['level'] > self::GUEST && $this->user['level'] < $minPermission)
+    if($_SESSION['usermanager_'.$this->fileMD5.'_level'] > self::GUEST && $this->getCurUserLevel() < $minPermission)
     {
       $error = true;
       $notice = $this->permissionFail;
@@ -223,25 +291,31 @@ class UserManager
   */
   function logout()
   {
-    $_SESSION['usermanager__level'] = 0;
-    $_SESSION['usermanager__username'] = '';
-    $_SESSION['usermanager__id'] = 0;
-    
-    $this->user = array(
-			'id' = $_SESSION['usermanager__id'],
-			'level' = $_SESSION['usermanager__level'],
-			'username' = $_SESSION['usermanager__username']
-		);
+    $_SESSION['usermanager_'.$this->fileMD5.'_level'] = 0;
+    $_SESSION['usermanager_'.$this->fileMD5.'_username'] = '';
+    $_SESSION['usermanager_'.$this->fileMD5.'_id'] = 0;
   }
   
   /*
   $minPermission: minimum permission level
-  returns true if current user has or exceeds
+  returns boolean if current user has or exceeds
   the minimum permission level passed.
   */
   function hasPerm($minPermission = self::USER)
   {
-    return $this->user['level'] >= $minPermission;
+    return $_SESSION['usermanager_'.$this->fileMD5.'_level'] >= $minPermission;
+  }
+  
+  /*
+  Does nothing by default
+  
+  Suggest extending this function in order to
+  format and unserialize the output
+  $user parameter is an object, each user field stored as an attribute
+  */
+  function unserializeUser($user)
+  {
+    return $user;
   }
   
   /*
@@ -271,7 +345,7 @@ class UserManager
     }
     echo '</tr>';
 
-  foreach ($this->getAllUsers() as $user) {
+  foreach ($this->simple->user as $user) {
     $this->unserializeUser($user); // This shouldn't need to actually return anything, since the object it being modified.
     echo '<tr valign="top">
     <td><input type="checkbox" name="user['.htmlentities($user['id']).'][delete]" value="true" /></td>
@@ -329,7 +403,7 @@ class UserManager
   class constants), $array is an associative array of all the
   values in the $userFields array.  Set $autocommit to false
   if you want to add more than one user at once.  If $autocommit
-  is false, you must call commitChanges() to update the underlying data.
+  is false, you must call commitDOMChanges() to update the XML file.
   
   returns false if username already exists, else returns true.
   
@@ -338,7 +412,29 @@ class UserManager
   */
   function addUser($username, $password, $level, $array, $autocommit = true)
   {
-    // NOT IMPLEMENTED AT THIS LEVEL
+    $usernameMatches = $this->lookupAttribute('username', $username);
+    if(count($usernameMatches) > 0)
+    return false;
+    $nextId = (int)$this->simple->nextid[0];
+    $this->simple->nextid[0] = $this->simple->nextid[0] + 1;
+            
+    $this->loadDom();
+    $root = $this->dom->documentElement;
+    $nextIdNode = $root->getElementsByTagName('nextid')->item(0);
+    $nextIdNode->nodeValue = $nextId+1;
+    $xUser = $root->appendChild($this->dom->createElement('user'));
+    $xUser->setAttribute('id', $nextId);
+    $xUser->appendChild($this->dom->createElement('username', $username));
+    $xUser->appendChild($this->dom->createElement('password', crypt($password)));
+    $xUser->appendChild($this->dom->createElement('level', $level));
+    foreach($this->userFields as $field)
+    {
+      $xUser->appendChild($this->dom->createElement($field, $array[$field]));
+    }
+    
+    if($autocommit)
+      $this->commitDOMChanges();
+    return true;
   }
   
   /*
@@ -349,7 +445,19 @@ class UserManager
   then their password will be changed, and the method will return true.  Else it will return false.
   */
   function changePassword($origPass, $newPass, $autocommit = true){
-  	// NOT IMPLEMENTED AT THIS LEVEL
+  	$this->loadDom();
+    $xpath = new DOMXPath($this->dom);
+    $xUser = $xpath->query('user[@id='.htmlentities($this->getCurUserID()).']')->item(0);
+    $xPassword = $xUser->getElementsByTagName('password')->item(0);
+    
+    if ($xPassword->nodeValue == crypt($origPass, $xPassword->nodeValue))
+    {
+      $xPassword->nodeValue = crypt($newPass);
+      if($autocommit)
+      	$this->commitDOMChanges();
+      return true;
+    }
+    return false;
   }
   
   /*
@@ -364,14 +472,36 @@ class UserManager
   class constants), $array is an associative array of all the
   values in the $userFields array.  Set $autocommit to false
   if you want to modify more than one user at once.  If $autocommit
-  is false, you must call commitChanges() to update the underlying data.
+  is false, you must call commitDOMChanges() to update the XML file.
   
   Suggest extending this function in order to
   serialize and format for storage custom parameters
   */
   function modifyUser($id, $username, $password, $level, $array, $autocommit = true)
   {
-    // NOT IMPLEMENTED AT THIS LEVEL
+    $this->loadDom();
+    $xpath = new DOMXPath($this->dom);
+    $xUser = $xpath->query('user[@id='.htmlentities($id).']')->item(0);
+    $xUsername = $xUser->getElementsByTagName('username')->item(0);
+    $xUsername->nodeValue = $username;
+    
+    if (trim($password) != '')
+    {
+      $xPassword = $xUser->getElementsByTagName('password')->item(0);
+      $xPassword->nodeValue = crypt($password);
+    }
+    
+    $xLevel = $xUser->getElementsByTagName('level')->item(0);
+    $xLevel->nodeValue = $level;
+    
+    foreach($this->userFields as $field)
+    {
+      $tag = $xUser->getElementsByTagName($field)->item(0);
+      $tag->nodeValue = $array[$field];
+    }
+    
+    if($autocommit)
+      $this->commitDOMChanges();
   }
   
   /*
@@ -379,37 +509,97 @@ class UserManager
   $autocommit: updates file when function executes
   deletes user with id passed.  Set $autocommit to false
   if you want to delete more than one user at once.  If $autocommit
-  is false, you must call commitChanges() to update the underlying data.
+  is false, you must call commitDOMChanges() to update the XML file.
   */
   function deleteUser($id, $autocommit = true)
   {
-    // NOT IMPLEMENTED AT THIS LEVEL
+    $this->loadDom();
+    $xpath = new DOMXPath($this->dom);
+    $xUser = $xpath->query('user[@id='.htmlentities($id).']')->item(0);
+    $xUser->parentNode->removeChild($xUser);
+    if($autocommit)
+      $this->commitDOMChanges();
   }
   
   /*
-  Commits changes made with autocommit set to false
+  Commits changes made to the file, and reloads
+  simplexml and dom.
   */
-  function commitChanges()
+  function commitDOMChanges()
   {
-    // NOT IMPLEMENTED AT THIS LEVEL
+    $this->dom->formatOutput = true;
+    unlink($this->file);
+    $this->dom->save($this->file);
+    $this->dom = null;
+    // $this->loadDom(); // I don't think this is necisary, since any use of $dom calls loadDom() first anyways
+    $this->simple = simplexml_load_file($this->file);
   }
   
   //////////////////
   // Private Functions
   //////////////////
   
-  /* Private function for manageUsers() to get all users */
-  function getAllUsers(){
-  	// NOT IMPLEMENTED AT THIS LEVEL
+  /* Private function for manageUsers() */
+  private function updateUsers()
+  {
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+      $maxId = (int)$this->simple->nextid;
+      $this->loadDom();
+      $root = $this->dom->documentElement;
+      
+     // Update entries
+      foreach ($root->getElementsByTagName('user') as $xUser)
+      {
+        $id = $xUser->getAttribute('id');
+        if (isset($_POST['user'][$id]))
+        {
+          $pUser = $_POST['user'][$id];
+          if(isset($pUser['delete']) && $pUser['delete'] == 'true') {
+            $root->removeChild($xUser);
+          }
+          else
+          {
+            $array = array();
+            foreach($this->userFields as $field)
+            {
+              $array[$field] = isset($pUser[$field]) ? $pUser[$field] : '';
+            }
+            $this->modifyUser($id, $pUser['username'], $pUser['password'], $pUser['level'], $array, false);
+          }
+        }
+      }
+      
+      // New Entries
+      foreach ($_POST['newuser'] as $pUser)
+      {
+        if(isset($pUser['username']) && trim($pUser['username']) != '' &&
+           isset($pUser['password']) && trim($pUser['password']) != '')
+        {
+          $array = array();
+          foreach($this->userFields as $field)
+          {
+            $array[$field] = isset($pUser[$field]) ? $pUser[$field] : '';
+          }
+          $this->addUser($pUser['username'], $pUser['password'], $pUser['level'], $array, false);
+        }
+      }
+      $this->commitDOMChanges();
+    }
   }
   
-  /* Private function for manageUsers() to handle updating the user data */
-  function updateUsers(){
-    // NOT IMPLEMENTED AT THIS LEVEL
-  }
-  
-  function hash($string,$salt=''){
-  	
+  /*
+  Loads DOM of xml file into memory on
+  pages that need it.
+  */
+  private function loadDom()
+  {
+    if(empty($this->dom))
+    {
+      $this->dom = new DOMDocument();
+      $this->dom->preserveWhiteSpace = false;
+      $this->dom->load($this->file);
+    }
   }
 }
 
