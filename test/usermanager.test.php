40cd96d0b8eeb5eda9f9d86654fbc7c3b12fb11b
Michael Diamond <dimo414@gmail.com>
2010-07-04 13:32:48 -0700

    Restructuring XMLUserManager

--- test/usermanager.test.php
+++ test/usermanager.test.php
@@ -1,60 +1,21 @@
 <?php
 
-require_once('../UserManager/UserManager.class.php');
+require_once('../UserManager/XMLUserManager.class.php');
 
 session_start();
-$_user = new UserManager('BasicUserManager.xml');
+$_user = new XMLUserManager('BasicUserManager.xml');
 
-$user = 'test';
-$pass = 'test';
-$level = UserManager::USER;
+// TEST HASH
+$hash =  $_user->hash('hello');
+$rehash = $_user->hash('hello',$hash);
 
-$_user->addUser($user,$pass,$level,array());
-$_user->login($user,$pass);
-
-$login_id = $_user->getCurUserID();
-if($login_id <= 0){
-	echo '<p>ERROR: Unexpected user ID</p>';
+if($hash == $rehash){
+	echo 'HASH WORKS<br />';
+} else {
+	echo "Hash didn't match:<br />
+".$hash.'<br />
+'.$rehash.'<br />';
 	exit;
 }
-
-$login_user = $_user->getCurUsername();
-if($user != $login_user){
-	echo '<p>ERROR: Unexpected username</p>';
-	exit;
-}
-
-$login_level = $_user->getCurUserLevel();
-if($level != $login_level){
-	echo '<p>ERROR: Unexpected level</p>';
-	exit;
-}
-
-$login_user_obj = $_user->getUser($login_id);
-// What to test?
-
-$_user->logout();
-
-if($_user->hasPerm($level)){
-	echo '<p>ERROR: Should have no permissions</p>';
-	exit;
-}
-
-$_user->loginSleep($user,$pass);
-
-if(!$_user->hasPerm($level)){
-	echo '<p>ERROR: Should have permission</p>';
-	exit;
-}
-
-$_user->changePassword($pass,$pass.$pass);
-
-$_user->logout();
-
-$_user->deleteUser($login_id);
-
-$_user->require_login();
-
-$_user->manageUsers();
 
 ?>
